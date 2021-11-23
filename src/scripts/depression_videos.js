c3da7586dc6f6c9b3bc7bced53d2aaa0bd04bbff
Leonardo Cunha <lmscunha@sga.pucminas.br>
2021-11-23 08:55:21 -0300

    feat: create depression_videos.js script.

---
+++ src/scripts/depression_videos.js
@@ -0,0 +1,25 @@
+if (window.localStorage.getItem('depression_videos') != null) {
+
+    const professionalsProfiles = JSON.parse(window.localStorage.getItem('depression_videos'))
+
+    for (const profile of professionalsProfiles) {
+        const newProfile = $('#professional-profile1').clone(true).appendTo($(".main-content"))
+
+        newProfile.find('#avatar').attr('src', `../assets/${profile.avatar}`)
+        newProfile.find('#name').text(profile.name)
+        newProfile.find('#profession').text(profile.profession)
+        newProfile.find('#site').attr('href', profile.site)
+        newProfile.find('#playlist').attr('href', profile.playlist)
+        newProfile.find('#about').text(profile.about)
+        newProfile.find('#address').text(profile.address)
+        newProfile.find('#phone').text(profile.phone)
+        newProfile.find('#email').text(profile.email)
+    }
+
+    $("#professional-profile1").fadeOut(1)
+    $("#professional-profile2").fadeOut(1)
+
+}
+
+
+
